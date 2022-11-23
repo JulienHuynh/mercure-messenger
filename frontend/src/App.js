ff2629aa1c2b9ecb7c215a5e4cd2eeefdb2ecb0c
Samuel Lunion <lunion.sam@gmail.com>
2022-11-23 10:31:46 +0100

    feat(/login): navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,31 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NeedAuth from "./Auth/NeedAuth";
 import UserList from "./Component/UserList";
+import Navbar from "./Component/Navbar";
 import Login from "./Auth/Login";
 import UserProvider from "./Context/UserContext";
 
 function App() {
-    return (
-        <UserProvider>
-            <BrowserRouter>
-                <Routes>
-                    <Route path='/' element={
-                        <NeedAuth>
-                            <UserList/>
-                        </NeedAuth>
-                    }/>
-                    <Route path='/login' element={<Login/>}/>
-                </Routes>
-            </BrowserRouter>
-        </UserProvider>
-    );
+  return (
+    <UserProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Navbar>
+                <NeedAuth>
+                  <UserList />
+                </NeedAuth>
+              </Navbar>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </UserProvider>
+  );
 }
 
 export default App;
